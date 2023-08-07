96248387cb48de240a703a5e0e6d929473fcf019
Brian Salomone <brian.j.salomone@gmail.com>
2023-08-07 12:18:21 -0700

    added hours

--- src/mainLayout.js
+++ src/mainLayout.js
@@ -3,6 +3,7 @@ const content = document.querySelector(".content")
 createMenu()
 createHeader()
 createIntro()
+createHours()
 
 function createMenu() {
     const div = document.createElement("div")
@@ -54,4 +55,31 @@ function createIntro() {
     parTwo.innerText = "At Cantibal's Feed Boat and Slurry House, we offer human-friendly alternatives for all you intra-species loving gourmands. Our menu is chock full of the dishes you grew up with (or came to know later, as the cravings overtook reason). We have cerebellum au gratin, tongue pâté, liver tartare, and even eyeball chowder - all sourced from local, home-grown vat people. These laboratory abominations of nature are 100% certified human-free, meeting the United Nations' definition of \"Non-Human Humanoid\". Best of all, they are entirely indistinguishable from real people!"
 
     parThree.innerText = "So come on down and try a chomp or two! We are located at the intersection of Wyatt and Herp, just off the 115 West, right where the old Arby's used to be."
+}
+
+function createHours() {
+    const div = document.createElement("div")
+    div.classList.add("hours")
+
+    content.append(div)
+
+    const hours = document.querySelector(".hours")
+
+    for (let i=0; i<7; i++) {
+        const par = document.createElement("p")
+        par.setAttribute("id", "day-" + (i+1) + ("-hours"))
+        hours.appendChild(par)
+    }
+
+    const days = document.querySelectorAll(".hours > p")
+
+    days[0].innerText = "Monday: 10:00 p.m. - 2:30 a.m."
+    days[1].innerText = "Tuesday: 10:00 p.m. - 2:30 a.m."
+    days[2].innerText = "Wednesday: 6:00 p.m. - 5:00 a.m."
+    days[3].innerText = "Thursday: 11:00 p.m. - 1:00 a.m."
+    days[4].innerText = "Friday: 12:00 a.m. - 12:30 a.m."
+    days[5].innerText = "Saturday: 12:15 a.m. - 12:20 a.m."
+    days[6].innerText = "Sunday: 5:00 a.m. - 5:00 p.m."
+
+    console.log(days[0])
 }
