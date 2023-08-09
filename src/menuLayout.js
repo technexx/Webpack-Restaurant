6301584fd57791680a9e56e738e97f45a8bfc83a
Brian Salomone <brian.j.salomone@gmail.com>
2023-08-09 11:40:36 -0700

    object fix

--- src/menuLayout.js
+++ src/menuLayout.js
@@ -10,15 +10,36 @@ export function createMenu() {
     const menuHeader = document.createElement("p")
     menuHeader.setAttribute("id", "menu-header")
     menuHeader.innerText = "Menu"
+    menu.appendChild(menuHeader)
 
     const itemOne = MenuItem("Cerebrum Au Gratin", "$32.69", "The juiciest, most executive part of the brain, slowly marinated and topped with Dahmer Chedder and eyecrust breadcrumbs.")
     const itemTwo = MenuItem("Tongue Pâté", "$26.92", "Vat-organic certified, gluten-free tongue minced with vegetables, marinated with our signature house Slurry Sauce, and served with Dutch bread.")
     const itemThree = MenuItem("Liver tartare", "54.99", "Tender liver, finely minced and mixed with our Mystery Mayhem Mix, served on a bed of pre-digested greens")
     const itemFour = MenuItem("Eyeball Chowder", "$28.50", "20/30 minimum pre-expiration vision eyeballs served in original ocular cavity with mixed vegetables")
 
-    menu.appendChild(menuHeader)
+    console.log(itemOne)
+
+    const menuItemsArray = [itemOne, itemTwo, itemThree, itemFour]
+    console.log(menuItemsArray)
+
+    for (let i=0; i<menuItemsArray.length; i++) {
+        const itemDiv = document.createElement("div")
+        itemDiv.classList.add("menu-item")
+
+        const itemPar = document.createElement("p")
+        const descriptionPar = document.createElement("p")
+        const pricePar = document.createElement("p")
+
+        itemPar.innerText = menuItemsArray[i].item
+        descriptionPar.innerText = menuItemsArray[i].description
+        pricePar.innerText = menuItemsArray[i].price
+
+        menu.appendChild(itemPar)
+        menu.appendChild(descriptionPar)
+        menu.appendChild(pricePar)
+    }
 }
 
-const MenuItem = () => {
+const MenuItem = (item, price, description) => {
     return {item, price, description}
 }
