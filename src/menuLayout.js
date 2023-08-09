90d01a53cbe9dafc2b5873d19536e55631a4e4ec
Brian Salomone <brian.j.salomone@gmail.com>
2023-08-09 10:52:52 -0700

    content alignment fix

--- src/menuLayout.js
+++ src/menuLayout.js
@@ -6,7 +6,19 @@ export function createMenu() {
     mainBody.appendChild(div)
 
     const menu = document.querySelector(".menu")
-    menu.innerText = "Menu"
 
-    console.log("imported from menu!")
+    const menuHeader = document.createElement("p")
+    menuHeader.setAttribute("id", "menu-header")
+    menuHeader.innerText = "Menu"
+
+    const itemOne = MenuItem("Cerebrum Au Gratin", "$32.69", "The juiciest, most executive part of the brain, slowly marinated and topped with Dahmer Chedder and eyecrust breadcrumbs.")
+    const itemTwo = MenuItem("Tongue Pâté", "$26.92", "Vat-organic certified, gluten-free tongue minced with vegetables, marinated with our signature house Slurry Sauce, and served with Dutch bread.")
+    const itemThree = MenuItem("Liver tartare", "54.99", "Tender liver, finely minced and mixed with our Mystery Mayhem Mix, served on a bed of pre-digested greens")
+    const itemFour = MenuItem("Eyeball Chowder", "$28.50", "20/30 minimum pre-expiration vision eyeballs served in original ocular cavity with mixed vegetables")
+
+    menu.appendChild(menuHeader)
+}
+
+const MenuItem = () => {
+    return {item, price, description}
 }
