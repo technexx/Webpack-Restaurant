74b07556263f231552b00ad36700418520e98bba
Brian Salomone <brian.j.salomone@gmail.com>
2023-08-09 19:12:52 -0700

    overflow fix on main page and new class for menu items in menu page

--- src/menuLayout.js
+++ src/menuLayout.js
@@ -10,17 +10,20 @@ export function createMenu() {
     const menuHeader = document.createElement("p")
     menuHeader.setAttribute("id", "menu-header")
     menuHeader.innerText = "Menu"
+
+    const menuItemsDiv = document.createElement("div")
+    menuItemsDiv.classList.add("menu-items")
+
     menu.appendChild(menuHeader)
+    menu.appendChild(menuItemsDiv)
 
     const itemOne = MenuItem("Cerebrum Au Gratin", "$32.69", "The juiciest, most executive part of the brain, slowly marinated and topped with Dahmer Chedder and eyecrust breadcrumbs.")
     const itemTwo = MenuItem("Tongue Pâté", "$26.92", "Vat-organic certified, gluten-free tongue minced with vegetables, marinated with our signature house Slurry Sauce, and served with Dutch bread.")
     const itemThree = MenuItem("Liver tartare", "54.99", "Tender liver, finely minced and mixed with our Mystery Mayhem Mix, served on a bed of pre-digested greens")
     const itemFour = MenuItem("Eyeball Chowder", "$28.50", "20/30 minimum pre-expiration vision eyeballs served in original ocular cavity with mixed vegetables")
 
-    console.log(itemOne)
-
+    const menuItems = document.querySelector(".menu-items")
     const menuItemsArray = [itemOne, itemTwo, itemThree, itemFour]
-    console.log(menuItemsArray)
 
     for (let i=0; i<menuItemsArray.length; i++) {
         const itemDiv = document.createElement("div")
@@ -34,9 +37,9 @@ export function createMenu() {
         descriptionPar.innerText = menuItemsArray[i].description
         pricePar.innerText = menuItemsArray[i].price
 
-        menu.appendChild(itemPar)
-        menu.appendChild(descriptionPar)
-        menu.appendChild(pricePar)
+        menuItems.appendChild(itemPar)
+        menuItems.appendChild(descriptionPar)
+        menuItems.appendChild(pricePar)
     }
 }
 
