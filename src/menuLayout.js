1ecb4e7307dfa93e95a57ffa37a99f513f8fd194
Brian Salomone <brian.j.salomone@gmail.com>
2023-08-10 11:12:05 -0700

    custom bg/border on menu items

--- src/menuLayout.js
+++ src/menuLayout.js
@@ -9,7 +9,7 @@ export function createMenu() {
 
     const menuHeader = document.createElement("p")
     menuHeader.setAttribute("id", "menu-header")
-    menuHeader.innerText = "Served All Day and Night, Excluding Quarantines and Raids"
+    menuHeader.innerText = "Served All Day and Night, Excluding Quarantine and Raid Hours"
 
     const menuItemsDiv = document.createElement("div")
     menuItemsDiv.classList.add("menu-items-holder")
@@ -19,8 +19,8 @@ export function createMenu() {
 
     const itemOne = MenuItem("Cerebrum Au Gratin", "$32.69", "The juiciest, most executive part of the brain, slowly marinated and topped with Dahmer Chedder and eyecrust breadcrumbs.")
     const itemTwo = MenuItem("Tongue Pâté", "$26.92", "Vat-organic certified, gluten-free tongue minced with vegetables, marinated with our signature house Slurry Sauce, and served with Dutch bread.")
-    const itemThree = MenuItem("Liver tartare", "54.99", "Tender liver, finely minced and mixed with our Mystery Mayhem Mix, served on a bed of pre-digested greens")
-    const itemFour = MenuItem("Eyeball Chowder", "$28.50", "20/30 minimum pre-expiration vision eyeballs served in original ocular cavity with mixed vegetables")
+    const itemThree = MenuItem("Liver tartare", "$54.99", "Tender liver, finely minced and mixed with our Mystery Mayhem Mix, served on a bed of pre-digested greens.")
+    const itemFour = MenuItem("Eyeball Chowder", "$28.50", "20/30 minimum pre-expiration vision eyeballs served in original ocular cavity with mixed vegetables.")
 
     const menuItemsHolder = document.querySelector(".menu-items-holder")
     const menuItemsArray = [itemOne, itemTwo, itemThree, itemFour]
