9df4a201fd5ed37d46455bde2595e8397e188178
Brian Salomone <brian.j.salomone@gmail.com>
2023-08-10 11:48:45 -0700

    main dishes in menu moved to sep. function

--- src/menuLayout.js
+++ src/menuLayout.js
@@ -11,21 +11,32 @@ export function createMenu() {
     menuHeader.setAttribute("id", "menu-header")
     menuHeader.innerText = "Served All Day and Night, Excluding Quarantine and Raid Hours"
 
+    menu.appendChild(menuHeader)
+
     const menuItemsDiv = document.createElement("div")
     menuItemsDiv.classList.add("menu-items-holder")
 
-    menu.appendChild(menuHeader)
     menu.appendChild(menuItemsDiv)
 
+    addMains()
+}
+
+function addMains() {
     const itemOne = MenuItem("Cerebrum Au Gratin", "$32.69", "The juiciest, most executive part of the brain, slowly marinated and topped with Dahmer Chedder and eyecrust breadcrumbs.")
     const itemTwo = MenuItem("Tongue Pâté", "$26.92", "Vat-organic certified, gluten-free tongue minced with vegetables, marinated with our signature house Slurry Sauce, and served with Dutch bread.")
     const itemThree = MenuItem("Liver tartare", "$54.99", "Tender liver, finely minced and mixed with our Mystery Mayhem Mix, served on a bed of pre-digested greens.")
     const itemFour = MenuItem("Eyeball Chowder", "$28.50", "20/30 minimum pre-expiration vision eyeballs served in original ocular cavity with mixed vegetables.")
 
-    const menuItemsHolder = document.querySelector(".menu-items-holder")
-    const menuItemsArray = [itemOne, itemTwo, itemThree, itemFour]
+    Dishes([itemOne, itemTwo, itemThree, itemFour])
 
-    for (let i=0; i<menuItemsArray.length; i++) {
+}
+
+const Dishes = (arrayOfMenuObjects) => {
+    const menuItemsHolder = document.querySelector(".menu-items-holder")
+    // const arrayOfMenuObjects = [itemOne, itemTwo, itemThree, itemFour]
+    console.log(arrayOfMenuObjects)
+
+    for (let i=0; i<arrayOfMenuObjects.length; i++) {
         const itemDiv = document.createElement("div")
         itemDiv.classList.add("menu-item")
 
@@ -37,9 +48,9 @@ export function createMenu() {
         description.setAttribute("id", "menu-item-description")
         price.setAttribute("id", "menu-item-price")
 
-        item.innerText = menuItemsArray[i].item
-        description.innerText = menuItemsArray[i].description
-        price.innerText = menuItemsArray[i].price
+        item.innerText = arrayOfMenuObjects[i].item
+        description.innerText = arrayOfMenuObjects[i].description
+        price.innerText = arrayOfMenuObjects[i].price
 
         itemDiv.appendChild(item)
         itemDiv.appendChild(description)
