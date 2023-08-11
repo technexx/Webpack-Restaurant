9d1f1506ae5a11aa7206c1d0beeb8eeccbe994fa
Brian Salomone <brian.j.salomone@gmail.com>
2023-08-11 11:31:07 -0700

    intro basics added

--- src/contactLayout.js
+++ src/contactLayout.js
@@ -5,6 +5,35 @@ export function createContact() {
     div.classList.add("contact")
     mainBody.appendChild(div)
 
-    const menu = document.querySelector(".contact")
-    menu.innerText = "Contact Page"
+    contactHeader()
+}
+
+function contactHeader() {
+    const contact = document.querySelector(".contact")
+
+    const contactHeader = document.createElement("p")
+    contactHeader.setAttribute("id", "contact-header")
+    contactHeader.innerText = "Contact Us!"
+
+    contact.appendChild(contactHeader)
+}
+
+function contactBody() {
+    const contact = document.querySelector(".contact")
+
+    const sectionOneHeader = document.createElement("p")
+    const sectionTwoHeader = document.createElement("p")
+    const sectionThreeHeader = document.createElement("p")
+    const sectionFourHeader = document.createElement("p")
+
+    sectionOneHeader.setAttribute("id", "contact-section-one-header")
+    sectionTwoHeader.setAttribute("id", "contact-section-two-header")
+    sectionThreeHeader.setAttribute("id", "contact-section-three-header")
+    sectionFourHeader.setAttribute("id", "contact-section-four-header")
+
+    sectionOneHeader.innerText = "General Inquiries"
+    sectionTwoHeader.innerText = "Health and Safety Concerns"
+    sectionThreeHeader.innerText = "Poison Control Referrals"
+    sectionFourHeader.innerText = "Charity"
+
 }
